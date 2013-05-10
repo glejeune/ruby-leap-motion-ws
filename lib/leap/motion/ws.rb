721d9a6f4325f9beb6b42eafc4436c1517a3486a
glejeune <gregoire.lejeune@gmail.com>
2013-05-10 12:08:48 +0200

    Tiny refactoring of Motion class

--- lib/leap/motion/ws.rb
+++ lib/leap/motion/ws.rb
@@ -6,55 +6,103 @@ require 'leap/motion/utils/history'
 
 module LEAP
   class Motion
+    # To interface with the Leap Motion, create a subclass of WS :
+    #
+    #   class MyLeap < LEAP::Motion::WS
+    #     def initialize(...)
+    #       #...
+    #     end
+    #
+    #     def on_frame(frame)
+    #       # ...
+    #     end
+    #
+    #     def on_connect
+    #       # ...
+    #     end
+    #
+    #     def on_disconnect
+    #       # ...
+    #     end
+    #
+    #     def on_error(message)
+    #       # ...
+    #     end
+    #   end
+    #
+    # on-frame is the only mandatory function.
     class WS
+      # Create a new WS instance
+      # 
+      # Avallables options are :
+      # * :uri : URI for the WebSocket (default: ws://127.0.0.1:6437)
+      # * :enable_gesture : boolean to indicate if we want to enable gesture or not (default: false)
+      # * :history_size : Size of the history (default: 1000)
       def initialize(options = {})
         @options = {:uri => "ws://127.0.0.1:6437", :enable_gesture => false, :history_size => 1000}.merge(options)
-        @gesture_enables = false
-        @ws = nil
         @history = LEAP::Motion::Utils::History.new(@options[:history_size])
       end
 
+      # Return true if gesture ar enabled, false otherwise
       def gestures?
-        @gesture_enables
+        options[:enable_gesture] 
       end
 
+      # Return the history
+      def history
+        @history ||= LEAP::Motion::Utils::History.new(options[:history_size])
+      end
+
+      # Enable gestures
       def gesture!
         unless @ws.nil?
           data = JSON "enableGestures" => true
-          @gesture_enables = @ws.send data
+          options[:enable_gesture] = ws.send data
         end
       end
 
+      # Start the interface
       def start(enable_gesture = false)
         EM.run do
-          @ws = WebSocket::EventMachine::Client.connect(:uri => @options[:uri])
-
-          @ws.onopen do
+          ws.onopen do
             on_connect if respond_to? :on_connect
-            gesture! if enable_gesture or @options[:enable_gesture]
+            gesture! if enable_gesture or options[:enable_gesture]
           end
 
-          @ws.onmessage do |msg, type|
+          ws.onmessage do |msg, type|
             message = JSON(msg)
             if message.key?("id") and message.key?("timestamp")
               frame = LEAP::Motion::WS::Frame.new(message)
-              @history << frame
+              history << frame
               on_frame frame
             end
           end
 
-          @ws.onerror do |err|
+          ws.onerror do |err|
             on_error(err) if respond_to? :on_error
           end
 
-          @ws.onclose do
+          ws.onclose do
             on_disconnect if respond_to? :on_disconnect
           end
         end
       end
 
+      # Stop the interface
       def stop
         EM::stop_event_loop
+      end
+
+      private
+
+      # Get options hash
+      def options
+        @options ||= {:uri => "ws://127.0.0.1:6437", :enable_gesture => false, :history_size => 1000}
+      end
+
+      # Return the WebSocket connection
+      def ws
+        @ws ||= WebSocket::EventMachine::Client.connect(:uri => options[:uri])
       end
     end
   end
---
+++ samples/motion_gesture_sample.rb
@@ -0,0 +1,38 @@
+$:.unshift "../lib"
+
+require 'leap-motion-ws'
+
+class LeapTest < LEAP::Motion::WS
+  def on_connect
+    puts "Connect"
+  end
+
+  def on_frame frame
+    puts "Frame ##{frame.id}, timestamp: #{frame.timestamp}, hands: #{frame.hands.size}, pointables: #{frame.pointables.size}" if frame.gestures.size > 0
+    frame.gestures.each do |gesture|
+      puts "  -> #{gesture.id} #{gesture.type} / #{gesture.state}"
+    end
+  end
+
+  def on_disconnect
+    puts "disconect"
+    stop
+  end
+end
+
+leap = LeapTest.new(:enable_gesture => true)
+
+Signal.trap("TERM") do
+  puts "Terminating..."
+  leap.stop
+end
+Signal.trap("KILL") do
+  puts "Terminating..."
+  leap.stop
+end
+Signal.trap("INT") do
+  puts "Terminating..."
+  leap.stop
+end
+
+leap.start
